0cff95ed69b35cd855e666f69c61c20460afe00c
Robin Repo Wecklauf <robinrepowecklauf@gmail.com>
2019-10-31 17:04:57 +0100

    Initial commit

--- git_automation.sh
+++ git_automation.sh
@@ -7,9 +7,9 @@ REPO_NAME=$1
 
 function create () {
 curl -H "Authorization: token $API" https://api.github.com -d '{"name": "'"${REPO_NAME}"'"}'
-cd /Users/robinrepowecklauf/Documents/my-projects/REPO_NAME
+cd /Users/robinrepowecklauf/Documents/my-projects/${REPO_NAME}
 git init
-git remote add origin git@github.com:robinrepowecklauf/$1.git
+git remote add origin git@github.com:robinrepowecklauf/${REPO_NAME}.git
 touch README.md
 git add .
 git commit -m "Initial commit"
